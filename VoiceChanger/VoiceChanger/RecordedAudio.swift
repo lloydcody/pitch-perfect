34af2657f15dce1d9e2696c0c862a6f084b2dc1e
lloydcody <lloydcody@gmail.com>
2015-04-04 21:42:58 +1100

    Committed by Udacity

---
+++ VoiceChanger/VoiceChanger/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  VoiceChanger
+//
+//  Created by Rhidian Cody on 21/03/2015.
+//  Copyright (c) 2015 Lloyd Cody. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio:NSObject {
+    var filePathUrl: NSURL!
+    var title: String!
+}
